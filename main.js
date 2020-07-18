4c3f555905487ce5091c3f7325cdffb81ae819de
imag-er <55343469+imag-er@users.noreply.github.com>
2020-07-18 19:08:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ function post(msg)
 var i;
 for (i=0;i<100;i++) {
 	post("再按"+(100-i)+"下lsm大佬就放你出去");
-	if (i==(Math.rand()*10))  post("看在你按了"+i+"下的份上就放你出去吧");
+	if (i==(Math.rand()*10)) {
+		post("看在你按了"+i+"下的份上就放你出去吧");
+		break;
+	}
 }
 	

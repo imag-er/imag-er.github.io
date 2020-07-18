917ffee1c89ec231e6752bad0bb66f0238bc846d
imag-er <55343469+imag-er@users.noreply.github.com>
2020-07-18 19:07:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-function dbg(str)
+function post(msg)
 {
-    console.log(str);
-}
-function prt(str)
-{
-    document.write(str);
+	window.alert(msg);
 }
 var i;
 for (i=0;i<100;i++) {
-	window.alert("再按"+(100-i)+"下lsm大佬就放你出去");
+	post("再按"+(100-i)+"下lsm大佬就放你出去");
+	if (i==(Math.rand()*10))  post("看在你按了"+i+"下的份上就放你出去吧");
 }
 	
